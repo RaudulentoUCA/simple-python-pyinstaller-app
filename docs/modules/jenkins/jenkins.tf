7fc64b8e42058a8ccb66b94cfa150365ce52692e
RaudulentoUCA <ra.arilopez@alum.uca.es>
2023-12-15 16:28:37 +0100

    jenkins funcional

--- docs/modules/jenkins/jenkins.tf
+++ docs/modules/jenkins/jenkins.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 3.0.2"
     }
   }
+}
+
+resource "docker_volume" "jenkins-docker-certs" {
+  name = "jenkins-docker-certs"
+}
+
+resource "docker_volume" "jenkins-data" {
+  name = "jenkins-data"
 }
 
 provider "docker" {
@@ -21,9 +29,36 @@ resource "docker_image" "jenkins_image" {
 }
 
 resource "docker_container" "jenkins_container" {
-  depends_on = [docker_image.jenkins_image]
-  name  = "jenkins_container"
-  image = docker_image.jenkins_image.name
+  depends_on   = [docker_image.jenkins_image]
+  name         = "jenkins_container"
+  image        = docker_image.jenkins_image.name
+  network_mode = "jenkins"
+  restart      = "on-failure"
+
+  env = [
+    "DOCKER_TLS_CERTDIR=/certs",
+  ]
+
+  volumes {
+    volume_name = docker_volume.jenkins-docker-certs.name
+    container_path = "/certs/client"
+  }
+
+  volumes {
+    volume_name = docker_volume.jenkins-data.name
+    container_path = "/var/jenkins_home"
+  }
+
+  volumes {
+    volume_name     = "host-home-volume"
+    container_path  = "/home"
+  }
+
+  command = [
+    "bash",
+    "-c",
+    "DOCKER_HOST=tcp://docker:2376 DOCKER_CERT_PATH=/certs/client DOCKER_TLS_VERIFY=1 JAVA_OPTS=-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true /usr/local/bin/jenkins.sh"
+  ]
 
   ports {
     internal = 8080
